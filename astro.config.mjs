4c47c0b43b0c8dd8617aa4a86a58b3383e476c00
elohimdjedje <mickaeldjedje7@gmail.com>
2025-03-14 02:11:55 +0100

    bonjour

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://elohimdjedje.github.io',
+  base: 'moi2',
+  trailingSlash: "always",
   integrations: [tailwind()],
 });
